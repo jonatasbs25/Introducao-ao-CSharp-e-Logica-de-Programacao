3d0ced0b83f17abe0d95efd745db975d786d1c10
Jônatas Borges de Souza <jonatasbsouza@hotmail.com>
2021-10-19 00:19:59 -0300

    Criação do primeiro projeto

---
+++ 1-primeiro-projeto/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //? Aplicações console são aquelas que são executadas no terminal
+
+            Console.WriteLine("Olá Mundo!"); //? Método para gravação de dados (Saída - Out)
+            Console.ReadLine(); //? Método de leitura de dados (Entrada - In)
+        }
+    }
+}
