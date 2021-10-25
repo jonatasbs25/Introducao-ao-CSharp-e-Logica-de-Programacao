ddbf31990aeb5066d3c88f238da01663d3cf7cab
Jônatas Borges de Souza <jonatasbsouza@hotmail.com>
2021-10-25 19:44:00 -0300

    Criação da pasta sobre tipos de dados

---
+++ 2-tipos-de-dados/TiposDeDados/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TiposDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
